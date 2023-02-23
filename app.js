06cb0d62a2ca1eaeb7ad6fe92d957bb8623b777f
Tyt-ngn <nganb2012231@student.ctu.edu.vn>
2023-02-23 10:46:07 +0700

    Cai dat cac handler truy xuat CSDL

--- app.js
+++ app.js
@@ -6,18 +6,18 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-app.use((req, res, next) => {
-    return next(new ApiError(404, "Resource not found"));
-});
-app.use((err, req, res, next) => {
-    return res.status(error.statusCode || 500).json({
-        message: error.message || "Internal Server Error",
-    });
-});
-
 app.get("/",(req, res) => {
     res.json({message: "Welcom to contact book application."});
 });
 app.use("/api/contacts", contactsRouter);
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
+});
+
+
 module.exports = app;
